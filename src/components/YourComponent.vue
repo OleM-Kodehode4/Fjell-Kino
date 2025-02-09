4af426441b20f119be39d49a91b536f178905a85
OleM-Kodehode4 <olemathias.kodehode@gmail.com>
2025-02-09 14:43:37 +0100

    Add Vue component and router setup for Fjell-Kino application

---
+++ src/components/YourComponent.vue
@@ -0,0 +1,7 @@
+<template>
+  <!-- ...existing code... -->
+  <img src="/logo.webp" alt="Logo">
+  <img src="/parasite.webp" alt="Parasite">
+  <img src="/interstellar.webp" alt="Interstellar">
+  <!-- ...existing code... -->
+</template>
